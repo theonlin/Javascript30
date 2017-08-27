904593b9793811e0e77cf6be4a37d06fb97936e4
Theon Lin <theon_lin@petple.co>
2017-08-27 20:30:14 +0800

    Select elements

---
+++ day11/scripts.js
@@ -0,0 +1,6 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
