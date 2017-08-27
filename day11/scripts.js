e5a1e3c512fe0c53dac9774b56ffe6c192b0811c
Theon Lin <theon_lin@petple.co>
2017-08-27 20:41:07 +0800

    Add Play/Pause function

--- day11/scripts.js
+++ day11/scripts.js
@@ -4,3 +4,21 @@ const progress = player.querySelector('.progress');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// functions
+function togglePlay() {
+	const method = video.paused ? 'play' : 'pause';
+	video[method]();
+}
+
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
+// events
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
