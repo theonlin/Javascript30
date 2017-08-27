834d517cab3834008b95695c67131876acec4b21
Theon Lin <theon_lin@petple.co>
2017-08-27 20:45:37 +0800

    Add Skip function

--- day11/scripts.js
+++ day11/scripts.js
@@ -16,9 +16,15 @@ function updateButton() {
 	toggle.textContent = icon;
 }
 
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // events
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
