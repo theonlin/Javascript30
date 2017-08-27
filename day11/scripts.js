c126bfdc5db2729c350347d21e2863198591a0d0
Theon Lin <theon_lin@petple.co>
2017-08-27 20:51:45 +0800

    Add range update function

--- day11/scripts.js
+++ day11/scripts.js
@@ -20,6 +20,10 @@ function skip() {
 	video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+}
+
 // events
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -28,3 +32,6 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
