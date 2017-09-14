1a0bd94ec39d4130d1a88f530e4ae72c3456188e
Theon Lin <theon_lin@petple.co>
2017-09-15 02:41:00 +0800

    Add function on buttons and input

--- day29/scripts.js
+++ day29/scripts.js
@@ -1,14 +1,17 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
+
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(seconds);
 	displayEndTime(then);
 
-	setInterval(() => {
+	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		if(secondsLeft <= 0) {
 			clearInterval(countdown);
@@ -35,3 +38,17 @@ function displayEndTime(timestamp) {
 	const minutes = end.getMinutes();
 	endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
